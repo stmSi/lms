e7d55fcd3c3a8fa19b7b4b84e81ed42bc6cc086f
stmSi <stm1998sithumyo@gmail.com>
2024-02-01 09:38:02 +0630

    use tera as template engine

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-use askama::Template;
+use std::{
+    collections::HashMap, fs::File, io::{Error, Read}, sync::Arc
+};
+
 use axum::{
     extract::Request,
+    http::StatusCode,
     middleware,
     middleware::Next,
-    response::{IntoResponse, Response},
-    routing::{get, post, get_service},
-    Router, http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::{get, get_service, post},
+    Router,
 };
+
+use tower_http::cors::{Any, CorsLayer};
 use tower_http::services::ServeDir;
-use tower_http::cors::{CorsLayer, Any};
+
+use tera::{Tera, Context};
 
 async fn trace_middleware(req: Request, next: Next) -> Result<impl IntoResponse, Response> {
     let span = tracing::info_span!("request", method = %req.method(), uri = %req.uri());
@@ -18,27 +25,25 @@ async fn trace_middleware(req: Request, next: Next) -> Result<impl IntoResponse,
     Ok(next.run(req).await)
 }
 
-#[derive(Template)]
-#[template(path = "base.html")]
-struct BaseTemplate<'a> {
-    title: &'a str,
-}
+#[tracing::instrument]
+async fn index_page(tera: Arc<Tera>) -> Result<impl IntoResponse, Response> {
+    let mut ctx = Context::new();
 
-#[derive(Template)]
-#[template(path = "home.html")]
-struct HomeTemplate<'a> {
-    name: &'a str,
+    let body = tera.render("index.html", &ctx).unwrap();
+    Ok(Html(body))
 }
 
 #[tracing::instrument]
-async fn home() -> HomeTemplate<'static> {
-    HomeTemplate { name: "World" }
+async fn login_page(tera: Arc<Tera>) -> Result<impl IntoResponse, Response> {
+    let mut ctx = Context::new();
+
+    let body = tera.render("login.html", &ctx).unwrap();
+    Ok(Html(body))
 }
 
 async fn clickMe() -> &'static str {
     "You clicked me!"
 }
-
 
 #[tokio::main]
 async fn main() {
@@ -51,13 +56,29 @@ async fn main() {
         .allow_origin(Any)
         .allow_headers(Any);
 
+    let tera = match Tera::new("templates/**/*") {
+        Ok(t) => Arc::new(t),
+        Err(e) => {
+            tracing::error!("Parsing error(s): {}", e);
+            std::process::exit(1);
+        }
+    };
+
     let app = Router::new()
-        .route("/", get(home))
-        .route("/click-me", post(clickMe))
+        .route("/", get({
+            let tera = tera.clone();
+            move || index_page(tera)
+        }))
+        .route("/login", get({
+            let tera = tera.clone();
+            move || login_page(tera)
+        }))
+        .route("/login", post(clickMe))
+        // .route("/test-navigation", get(test_navigation))
         .nest_service("/static", ServeDir::new("static"))
         .layer(middleware::from_fn(trace_middleware))
         .layer(cors);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:6969").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
